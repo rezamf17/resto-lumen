25b2113932999d3c4b2bbedc190cc65dc45bcab5
rezamf17 <rezayuyara@gmail.com>
2022-02-24 14:47:38 +0700

    add vuetify

--- src/main.js
+++ src/main.js
@@ -3,13 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import axios from 'axios'
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 axios.defaults.baseURL = "http://localhost:3000/"
-axios.defaults.headers.common['Authorization'] = 'bearer' + localStorage.getItem('token') 
+axios.defaults.headers.common['Authorization'] = 'bearer' + localStorage.getItem('token')
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,36 +1,15 @@
 <template>
-  <div class="home">
-    <h3>Menu Pages</h3>
-    <table>
-      <thead>
-        <th>Menu Name</th>
-        <th>Category</th>
-        <th>Price</th>
-        <th>Actions</th>
-      </thead>
-      <tbody v-for="item in menu" :key="item.id">
-        <td>{{item.name}}</td>
-        <td>{{item.category.category_name}}</td>
-        <td>{{item.price}}</td>
-        <td>Order</td>
-      </tbody>
-    </table>
-  </div>
+  <hello-world />
 </template>
 
 <script>
-// @ is an alias to /src
-import axios from 'axios'
-export default {
-  name: 'Home',
-  data(){
-    return{
-      menu : ''
-    }
-  },
-  async created(){
-    const response = await axios.get('menu')
-    this.menu = response.data.data_menu
-  },
-}
+  import HelloWorld from '../components/HelloWorld'
+
+  export default {
+    name: 'Home',
+
+    components: {
+      HelloWorld,
+    },
+  }
 </script>
