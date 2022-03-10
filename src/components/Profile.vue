1ff4670babdf049291639ef6660a62c82e3117c9
rezamf17 <rezayuyara@gmail.com>
2022-03-10 22:15:37 +0700

    show data profile with vuex

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -17,11 +17,11 @@
                         <v-list-item-content>
                         <v-avatar>
                         <img
-                            :src="`http://localhost:3000/avatar/${profile.image.image_name}`"
+                            :src="`http://localhost:3000/avatar/${user.image.image_name}`"
                             :alt="image"
                         >
                         </v-avatar>
-                        {{profile.image.image_name}}
+                        {{user.image.image_name}}
                         </v-list-item-content>
                         <v-list-item-content>
                             <v-btn text router to="/profilePict">
@@ -34,7 +34,7 @@
                         <v-list-item-content class="align-end">
                             <v-text-field
                             label="Name"
-                            v-model="profile.name"></v-text-field>
+                            v-model="user.name"></v-text-field>
                         </v-list-item-content>
                     </v-list-item>
                     <v-list-item>
@@ -42,13 +42,13 @@
                         <v-list-item-content class="align-end">
                             <v-text-field
                             label="Email"
-                            v-model="profile.email"></v-text-field>
+                            v-model="user.email"></v-text-field>
                         </v-list-item-content>
                     </v-list-item>
                     <v-list-item>
                         <v-list-item-content>Level User :</v-list-item-content>
                         <v-list-item-content class="align-end">
-                            {{profile.level}}
+                            {{user.level}}
                         </v-list-item-content>
                     </v-list-item>
                 </v-list>
@@ -65,6 +65,7 @@
 <script>
 import axios from 'axios'
 import swal from 'sweetalert'
+import {mapGetters} from 'vuex'
 export default {
     data(){
         return{
@@ -76,14 +77,10 @@ export default {
                     ],
         }
     },
+    computed:{
+        ...mapGetters(['user'])
+    },
     methods:{
-        getProfile(){
-            axios.get("api/profile").then((res) => {
-                this.profile = res.data.user
-            }).catch((e) => {
-                console.log(e)
-            })
-        },
         updateProfile(){
             return axios.put('api/profile/' + this.profile.id, {
                 name : this.profile.name,
@@ -98,7 +95,6 @@ export default {
                     icon: "success",
                 });
                 this.dialog = true
-                this.getProfile()
             }).catch((e) => {
                 console.log(e)
             })
@@ -110,9 +106,6 @@ export default {
         onDisabled(){
             this.disable = !this.disable
         }
-    },
-    mounted(){
-        this.getProfile()
     },
 }
 </script>
